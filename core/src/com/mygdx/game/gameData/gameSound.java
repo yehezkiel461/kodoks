fbc6a826e92256401e6c0ed225310131dcb34ee4
Misael Yosa <107090127+misaelyosa@users.noreply.github.com>
2023-06-21 16:16:32 +0700

    Update gameSound.java

--- core/src/com/mygdx/game/gameData/gameSound.java
+++ core/src/com/mygdx/game/gameData/gameSound.java
@@ -1,4 +1,10 @@
 package com.mygdx.game.gameData;
 
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
+
 public class gameSound {
+
+    public Sound hitSound;
+    public Music gameMusic;
 }
