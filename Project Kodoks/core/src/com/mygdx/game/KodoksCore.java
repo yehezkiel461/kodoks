0eeb68f7a5ea68ff8e8e3b9a7e7ce9b933de2725
Misael Yosa <107090127+misaelyosa@users.noreply.github.com>
2023-06-21 12:06:32 +0700

    asset moving
    gitu

--- Project Kodoks/core/src/com/mygdx/game/KodoksCore.java
+++ Project Kodoks/core/src/com/mygdx/game/KodoksCore.java
@@ -5,27 +5,28 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
-public class MyGdxGame extends ApplicationAdapter {
+public class KodoksCore extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
+
+	Texture background;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		background = new Texture("background.png");
 	}
 
 	@Override
 	public void render () {
 		ScreenUtils.clear(1, 0, 0, 1);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(background, 0, 0);
 		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		background.dispose();
 	}
 }
