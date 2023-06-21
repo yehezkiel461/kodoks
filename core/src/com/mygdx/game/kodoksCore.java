e1a43273095bddfcfce44df44d745dfe4c252b7a
Kean Owen <venderjsn@gmail.com>
2023-06-21 15:54:18 +0700

    bikin Package
    buat object dan KODOK

--- core/src/com/mygdx/game/kodoksCore.java
+++ core/src/com/mygdx/game/kodoksCore.java
@@ -6,10 +6,9 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.utils.ScreenUtils;
 
-public class MyGdxGame extends ApplicationAdapter {
+public class kodoksCore extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
 	Texture backgroundTexture;
