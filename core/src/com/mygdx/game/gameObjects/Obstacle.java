485119f24c3c24c5afd361c1f88a3259d83ff38b
Kean Owen <venderjsn@gmail.com>
2023-06-21 16:16:46 +0700

    create inside Obstacle

---
+++ core/src/com/mygdx/game/gameObjects/Obstacle.java
@@ -0,0 +1,9 @@
+package com.mygdx.game.gameObjects;
+
+public abstract class Obstacle {
+    public int x;
+    public int y;
+    public int width;
+    public int height;
+    public int speed;
+}
